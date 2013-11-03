a0125c66a0dd922284b8d8010e9c6a142fc5d767
Phoenix Nemo <mrphoenixlzx@gmail.com>
2013-11-03 22:54:43 +0800

    increase queryLimit to 100

--- record-mysql.js
+++ record-mysql.js
@@ -4,7 +4,8 @@ var pool  = mysql.createPool({
     host     : config.mysqlhost,
     database : config.mysqldb,
     user     : config.mysqluser,
-    password : config.mysqlpass
+    password : config.mysqlpass,
+    connectionLimit: 100
 });
 
 pool.getConnection(function(err, connection) {
@@ -16,6 +17,10 @@ pool.getConnection(function(err, connection) {
         connection.query('SELECT * from `records` WHERE `name` = ? AND (`type` = ? OR `type` = "CNAME")',
             [name, type],
             function(err, result) {
+                // Set timeout as node-mysql still implementing this.
+                var timer = setTimeout(function() {
+                    connection.release();
+                }, 100)
                 if (err) {
                     connection.release();
                     return callback(err, null);
